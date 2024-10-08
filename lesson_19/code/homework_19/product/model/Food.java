37805aa51c1a4af3127acd12d1cb578b896ab28f
Diana <sundiana81@dmail.com>
2024-10-08 21:11:48 +0200

    Project update

---
+++ lesson_19/code/homework_19/product/model/Food.java
@@ -0,0 +1,36 @@
+package homework_19.product.model;
+
+import java.util.Objects;
+
+public class Food extends Product{
+
+    private String expDate;
+
+      // delaem construktor
+
+    public Food(double price, String name, long barCode, String expDate) {
+        super(price, name, barCode);
+        this.expDate = expDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Food{" +
+                "expDate='" + expDate + '\'' +
+                "} " + super.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Food food)) return false;
+        if (!super.equals(o)) return false;
+        return Objects.equals(expDate, food.expDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), expDate);
+    }
+}
+
---
+++ lesson_19/code/homework_19/product/model/MarketAppl.java
@@ -0,0 +1,32 @@
+package homework_19.product.model;
+
+public class MarketAppl {
+
+    public static void main(String[] args) {
+
+        Product[] products = new Product[3];
+        products[0] = new MeatFood(35.7, "Fisch", 8473639L, "24-10-24", "Fisch");
+        products[1] = new MilkFood(3.0, "Milk", 8373637281L, "24-10-24", "Zige", 3.5);
+        products[2] = new Food(1.0, "Bread", 5748395L, "31-12-24");
+
+        printAllProducts(products);
+        double totalPrice = calculateTotalPrice(products);
+        System.out.println("Total price of all products: " + totalPrice);
+    }
+
+
+    public static void printAllProducts(Product[] products) {
+        for (Product product : products) {
+            System.out.println(product);
+        }
+    }
+
+
+    public static double calculateTotalPrice(Product[] products) {
+        double total = 0;
+        for (Product product : products) {
+            total += product.getPrice();
+        }
+        return total;
+    }
+}
